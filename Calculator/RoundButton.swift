eb630eecb1fa08c9b9b68ba97df0fdb21a8c5a55
Yury Ulasevich <yura.ulasevich@gmail.com>
2020-01-09 00:17:53 +0300

    UI without autolayout

---
+++ Calculator/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by Юра Уласевич on 1/8/20.
+//  Copyright © 2020 Юра Уласевич. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Юра Уласевич on 1/8/20.
+//  Copyright © 2020 Юра Уласевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
